bf7d862601013d518fb63a892d625b1c1f03794e
kimseonju <tjsdkdlel199@naver.com>
2026-09-03 20:07:15 +0900

    release: prepare MoaLog v0.1.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 30                     // createTrashRequest · GENERATION_* · setRequireOriginal
         targetSdk = 35
         versionCode = 1
-        versionName = "0.1"
+        versionName = "0.1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
